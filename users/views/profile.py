4207650d6069a17e1e3670f21ed45b7cdb8b8369
Thulasiram <thulasi503@gmail.com>
2017-02-19 13:24:27 +0530

    Partially working user profile

--- users/views/profile.py
+++ users/views/profile.py
@@ -1,40 +1,52 @@
 from string import Template
 
 from django import forms
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
 from django.template.response import TemplateResponse
 from django.utils.safestring import mark_safe
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.forms import widgets, ClearableFileInput
+from django.forms import widgets, ClearableFileInput, TextInput
+from rest_framework import serializers
 
 
-class PictureWidget(widgets.Widget):
-    def render(self, name, value, attrs=None):
-        html = Template("""<img src="$link"/>""")
-        return mark_safe(html.substitute(link=value))
+class ProfileSerializer(serializers.Serializer):
+    first_name = serializers.CharField(max_length=100,
+                                       required=False,
+                                       error_messages={
+                                           'required': 'First name is required',
+                                           'blank': 'First name cannot be blank',
+                                       })
+    last_name = serializers.CharField(max_length=100,
+                                      required=False,
+                                      error_messages={
+                                          'required': 'Last name is required',
+                                          'blank': 'Last name cannot be blank',
+                                      })
+    photo = serializers.ImageField(required=False)
+    phone = serializers.IntegerField(min_value=(10 ** 9),
+                                     max_value=(10 ** 10 - 1),
+                                     error_messages={
+                                         'required': 'Phone number is required',
+                                         'blank': 'Phone number cannot be blank',
+                                         'min_value': 'Invalid phone number',
+                                         'max_value': 'Invalid phone number',
+                                         'invalid': 'Invalid phone number'
+                                     })
 
-class MLPW(widgets.MultiWidget):
-    def __init__(self):
-        _widgets = [ClearableFileInput, PictureWidget]
-        super(MLPW, self).__init__(_widgets)
-
-    def decompress(self, value):
-        if value:
-            return [value.day, value.month, value.year]
-        return [None, None, None]
-
-class ProfileForm(forms.Form):
-    image = forms.ImageField(label='Change profile pic', widget=MLPW)
-    name = forms.CharField()
-    email = forms.EmailField()
 
 class Profile(LoginRequiredMixin, TemplateView):
     template_name = 'users/profile.html'
 
     def get(self, request, *args, **kwargs):
-        return TemplateResponse(request, self.template_name, {'form': ProfileForm})
+        context = {'profile_url': request.user}
+        return TemplateResponse(request, self.template_name, context=context)
 
     def post(self, request):
-        return None
+        data = request.POST
+        serializer = ProfileSerializer(data=data)
+        if serializer.is_valid():
+            context = {'profile_url': request.user}
+        else:
+            context = {'form_errors': serializer.errors}
+        return TemplateResponse(request, self.template_name, context=context)
